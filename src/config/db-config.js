22314debe4816e6626cb8a0458e6aebf4012f3bc
Antonio <aloncar.faks@gmail.com>
2019-10-02 00:43:58 +0200

    removed

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -1,5 +1,4 @@
 module.exports = {
     url:  "mongodb://192.168.99.100:32771/mmdb",
-    test_url: "mongodb://192.168.99.100:32771/test_mmdb",
-    test_db_name: "test_mmdb"
+    test_url: "mongodb://192.168.99.100:32771/test_mmdb"
 }
