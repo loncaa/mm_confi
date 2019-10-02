848c0b4deaa3e640882bef45784f19af48b6efb0
Antonio <aloncar.faks@gmail.com>
2019-10-02 23:22:46 +0200

    added attendees controller, renamed router to controller

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -1,6 +1,7 @@
-var authorizationRouter = require('./authorization-controller');
-var usersRouter = require('./users-controller');
-var bookingsRouter = require('./bookings-controller')
+var authorizationController = require('./authorization-controller');
+var usersController = require('./users-controller');
+var bookingsController = require('./bookings-controller')
+var attendeesController = require('./attendees-controller')
 
 const jwtConfig = require('../config/jwt-config')
 const jwtCheckMiddleware = require('../middleware/jwt-check-middleware')
@@ -10,9 +11,10 @@ function connectRoutes(path, app) {
     //Check jwt token middleware
     app.use(jwtCheckMiddleware().unless({path: jwtConfig.jwtIgnoreUrls}))
 
-    app.use(path, authorizationRouter)
-    app.use(path, usersRouter)
-    app.use(path, bookingsRouter)
+    app.use(path, authorizationController)
+    app.use(path, usersController)
+    app.use(path, bookingsController)
+    app.use(path, attendeesController)
 }
 
 module.exports = {
